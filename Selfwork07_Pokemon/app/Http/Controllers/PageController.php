7100f0af0dd62aeacc5d25c519281699eddbdbe3
Alberto Cuogo <cuogoalberto@gmail.com>
2023-05-23 15:33:48 +0200

    sistemato url pokemon.show

--- Selfwork07_Pokemon/app/Http/Controllers/PageController.php
+++ Selfwork07_Pokemon/app/Http/Controllers/PageController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class PageController extends Controller
 {
     public function index() {
         $pokemons = Http::get('https://pokeapi.co/api/v2/pokemon')->json();
+        foreach ($pokemons['results'] as $pokemon) {
+            $id = Str::afterLast($pokemon['url'], 'https://pokeapi.co/api/v2/pokemon/');
+            $data[] = [
+                'name' => $pokemon['name'],
+                'id' => (int)$id
+            ];
+        }
 
-        return view('pokemon.index', ['pokemons' => $pokemons]);
+        return view('pokemon.index', ['pokemons' => $pokemons], ['pokemon' => $data]);
     }
 
     public function show($id) {
-        $id_last = basename(parse_url($id, PHP_URL_PATH));
-        $pokemon = Http::get('https://pokeapi.co/api/v2/pokemon/' . $id_last)->json();
+        $pokemon = Http::get('https://pokeapi.co/api/v2/pokemon/' . $id)->json();
 
         return view('pokemon.show', ['pokemon' => $pokemon]);
     }
--- Selfwork07_Pokemon/resources/views/pokemon/index.blade.php
+++ Selfwork07_Pokemon/resources/views/pokemon/index.blade.php
@@ -1,11 +1,12 @@
 <x-main>
     <h1 class="nes-text is-primary">TEST</h1>
 
-    @foreach ($pokemons['results'] as $pokemon)
+
+    @foreach ($pokemon as $item)
 
         <div class="nes-container m-5 rounded">
-            <p class="title text-capitalize">{{ $pokemon['name'] }}</p>
-            <a class="nes-btn is-primary" href="{{route('pokemon.show',['id' => $pokemon['url']])}}">INFO</a>
+            <p class="title text-capitalize">{{ $item['name'] }}</p>
+            <a class="nes-btn is-primary" href="{{route('pokemon.show',['id' => $item['id']])}}">INFO</a>
         </div>
     @endforeach
 </x-main>
